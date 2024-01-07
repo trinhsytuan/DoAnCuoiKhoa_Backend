c5274c596f1839efef26ad9b3479507cc43a0513
Tuan <trinhsytuan@gmail.com>
2024-01-07 10:05:22 +0700

    edit code

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -13,16 +13,6 @@ const URL_BASE = {
 const recordNewUpdate = { new: true }; // Trả về tài liệu mới sau khi cập nhật
 
 const sort_time = { createdAt: -1 };
-const TYPE_IMAGE_CAP_MOI = {
-    ANH_KHU_DAT: "anh-khu-dat",
-    DON_DANG_KY: "don-dang-ky",
-    CHUNG_TU_NGHIA_VU_TAI_CHINH: "chung-tu-nghia-vu-tai-chinh",
-    HOP_DONG: "hop-dong",
-    OTHER: "other",
-};
-const TYPE_IMAGE_CAP_LAI = {
-    ANH_KHU_DAT: "anh-khu-dat",
-};
 const STATUS_TD = {
     reject: "reject",
     accepted: "accepted",
@@ -39,9 +29,7 @@ module.exports = {
     WRONG_PASSWORD,
     URL_BASE,
     recordNewUpdate,
-    TYPE_IMAGE_CAP_MOI,
     sort_time,
-    TYPE_IMAGE_CAP_LAI,
     STATUS_TD,
     CHUA_DU_THONG_TIN
 
