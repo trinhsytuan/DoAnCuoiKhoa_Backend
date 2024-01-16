95b738b813e96908c295df79945d878fa730e32b
Tuan <trinhsytuan@gmail.com>
2024-01-16 17:13:59 +0700

    Update category and add MaHoa

--- src/controller/category/categoryController.js
+++ src/controller/category/categoryController.js
@@ -1,56 +1,67 @@
-const {handleCreate, handleDelete, handleUpdate} = require("../../config/baseChung");
-const {categoryModels} = require("../../model/categoryModel");
+const {
+  handleCreate,
+  handleDelete,
+  handleUpdate,
+} = require("../../config/baseChung");
+const { categoryModels } = require("../../model/categoryModel");
 
 const insertOneCategory = async (req, res) => {
-    try {
-        const data = req.body;
+  try {
+    const data = req.body;
 
-        const response = await handleCreate(categoryModels, {
-            ...data,
-            idUser: req.decodeToken._id,
-        });
-        return res.status(200).json(response);
-    } catch (e) {
-        return res.status(400).json({
-            success: false,
-            message: e.toString()
-        })
-    }
+    const response = await handleCreate(categoryModels, {
+      ...data,
+      idUser: req.decodeToken._id,
+    });
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      message: e.toString(),
+    });
+  }
 };
 
 const deleteCategory = async (req, res) => {
-    try {
-        const {id} = req.params;
-        const response = await handleDelete(categoryModels, id);
-        return res.status(200).json(response);
-    } catch (e) {
-        return res.status(400).json({
-            success: false,
-            message: e.toString()
-        })
-    }
+  try {
+    const { id } = req.params;
+    const response = await handleDelete(categoryModels, id);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      message: e.toString(),
+    });
+  }
 };
 const editCategory = async (req, res) => {
-    try {
-        const {id} = req.params;
-        const response = await handleUpdate(categoryModels, id, req.body);
-        return res.status(200).json(response);
-    } catch (e) {
-        return res.status(400).json({
-            success: false,
-            message: e.toString()
-        })
-    }
-}
+  try {
+    const { id } = req.params;
+    const response = await handleUpdate(categoryModels, id, req.body);
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      message: e.toString(),
+    });
+  }
+};
 const getCategoryByID = async (req, res) => {
-    try {
-        const response = await categoryModels.findAll({idUser: req.decodeToken._id}).loan();
-        return res.status(200).json(response);
-    } catch (e) {
-        return res.status(400).json({
-            success: false,
-            message: e.toString()
-        })
-    }
-}
-module.exports = {insertOneCategory, deleteCategory, editCategory, getCategoryByID};
+  try {
+    const response = await categoryModels
+      .find({ idUser: req.decodeToken._id })
+      .lean();
+    return res.status(200).json(response);
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      message: e.toString(),
+    });
+  }
+};
+module.exports = {
+  insertOneCategory,
+  deleteCategory,
+  editCategory,
+  getCategoryByID,
+};
