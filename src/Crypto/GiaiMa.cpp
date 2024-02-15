d64fd5a2e75d3a19d318e5d379174faac78fbd21
Tuan <trinhsytuan@gmail.com>
2024-02-15 16:53:14 +0700

    Update crypto

---
+++ src/Crypto/GiaiMa.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include<pbc.h>
+#include<stdio.h>
+#include<fstream>
+#include <sstream>
+#include<vector>
+#define maxn 200
+using namespace std;
+pairing_t pairing;
+#define PBC_CONVERT_BASE 10
+struct global_parameters
+{
+    element_t g;
+    element_t alpha;
+    element_t params[2*maxn+10];
+    element_t v;
+    element_t y;
+    element_t d[2*maxn+10];
+};
+
+void SETUP_PARAMS ()
+{
+    FILE* file = fopen("output_pairing.txt", "rb");
+    char param[1024];
+    size_t  count = fread(param, 1, 9999,  file);
+    if (!count) pbc_die("input error");
+    pairing_init_set_buf(pairing, param, count);
+    if (!pairing_is_symmetric(pairing)) pbc_die("pairing must be symmetric");
+}
+global_parameters paramsGlobal;
+element_t Hdr[2];
+vector<int>slgm;
+char* readFile() {
+    string str;
+    getline(cin,str);
+    char* buffer = new char[str.length() + 1];
+    strcpy(buffer, str.c_str());
+    return buffer;
+}
+char* convertStringToChar(string str) {
+
+    char* buffer = new char[str.length() + 1];
+    strcpy(buffer, str.c_str());
+    return buffer;
+}
+void setup() {
+    element_init_G1(paramsGlobal.g, pairing);
+    element_set_str(paramsGlobal.g, readFile(), PBC_CONVERT_BASE);
+    element_init_Zr(paramsGlobal.alpha, pairing);
+    element_set_str(paramsGlobal.alpha, readFile(), PBC_CONVERT_BASE);
+    for(int i = 0; i <= 2*maxn;i++) {
+        element_init_G1(paramsGlobal.params[i], pairing);
+        element_set_str(paramsGlobal.params[i], readFile(), PBC_CONVERT_BASE);
+
+    }
+    element_init_Zr(paramsGlobal.y, pairing);
+    element_set_str(paramsGlobal.y, readFile(), PBC_CONVERT_BASE);
+    element_init_G1(paramsGlobal.v, pairing);
+    element_set_str(paramsGlobal.v, readFile(), PBC_CONVERT_BASE);
+    element_clear(paramsGlobal.params[maxn+1]);
+}
+void xulydauvao(string s) {
+    stringstream ss(s);
+    int i;
+    while (ss >> i) {
+        slgm.push_back(i);
+        if (ss.peek() == ',')
+            ss.ignore();
+    }
+}
+void Decryption(int PERSON, string khoabimat, string Hdr1, string Hdr2) {
+    element_t tu, mau, temp, knew;
+    element_init_GT(tu,pairing);
+    element_init_G1(Hdr[0], pairing);
+    element_init_G1(Hdr[1], pairing);
+    element_set_str(Hdr[0], convertStringToChar((Hdr1)), PBC_CONVERT_BASE);
+
+    element_set_str(Hdr[1], convertStringToChar((Hdr2)), PBC_CONVERT_BASE);
+    pairing_apply(tu, paramsGlobal.params[PERSON], Hdr[1], pairing);
+
+    element_init_G1(temp, pairing);
+    element_set_str(temp, convertStringToChar(khoabimat), PBC_CONVERT_BASE);
+
+    for (int i=0;i<slgm.size();i++)
+    {
+        int j=slgm[i];
+        if (j==PERSON) continue;
+        element_mul(temp,temp,paramsGlobal.params[maxn+1-j+PERSON]);
+    }
+    element_init_GT(mau, pairing);
+    pairing_apply(mau, temp, Hdr[0], pairing);
+    element_init_GT(knew, pairing);
+    element_div(knew,tu,mau);
+    element_printf("%B", knew);
+
+}
+int main(int argc, char const *argv[]) {
+    //Quy ước số 1 là tập giải mã
+    string khoabimat = "[" + string(argv[argc-6]) + argv[argc-5] + "]";
+    freopen("dauvao.txt", "rt", stdin);
+    //Nguoi giai ma
+    //Khoa bi mat cua nguoi nhan
+    //Ban ma Hdr1, Hdr2
+    SETUP_PARAMS();
+    setup();
+    string Hdr1 = "[" + string(argv[argc-4]) + argv[argc-3] + "]";
+    string Hdr2 = "[" + string(argv[argc-2]) + argv[argc-1] + "]";
+    Decryption(stoi(argv[argc-7]), khoabimat, Hdr1, Hdr2);
+
+}
---
+++ src/Crypto/MaHoaT.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include<pbc.h>
+#include<stdio.h>
+#include<fstream>
+#include <sstream>
+#include<vector>
+#define maxn 200
+using namespace std;
+pairing_t pairing;
+#define PBC_CONVERT_BASE 10
+struct global_parameters
+{
+    element_t g;
+    element_t alpha;
+    element_t params[2*maxn+10];
+    element_t v;
+    element_t y;
+    element_t d[2*maxn+10];
+};
+
+void SETUP_PARAMS ()
+{
+    FILE* file = fopen("output_pairing.txt", "rb");
+    char param[1024];
+    size_t  count = fread(param, 1, 9999,  file);
+    if (!count) pbc_die("input error");
+    pairing_init_set_buf(pairing, param, count);
+    if (!pairing_is_symmetric(pairing)) pbc_die("pairing must be symmetric");
+}
+global_parameters paramsGlobal;
+element_t Hdr[2];
+vector<int>slgm;
+char* readFile() {
+    string str;
+    getline(cin,str);
+    char* buffer = new char[str.length() + 1];
+    strcpy(buffer, str.c_str());
+    return buffer;
+}
+void setup() {
+    element_init_G1(paramsGlobal.g, pairing);
+    element_set_str(paramsGlobal.g, readFile(), PBC_CONVERT_BASE);
+    element_init_Zr(paramsGlobal.alpha, pairing);
+    element_set_str(paramsGlobal.alpha, readFile(), PBC_CONVERT_BASE);
+    for(int i = 0; i <= 2*maxn;i++) {
+        element_init_G1(paramsGlobal.params[i], pairing);
+        element_set_str(paramsGlobal.params[i], readFile(), PBC_CONVERT_BASE);
+
+    }
+    element_init_Zr(paramsGlobal.y, pairing);
+    element_set_str(paramsGlobal.y, readFile(), PBC_CONVERT_BASE);
+    element_init_G1(paramsGlobal.v, pairing);
+    element_set_str(paramsGlobal.v, readFile(), PBC_CONVERT_BASE);
+    element_clear(paramsGlobal.params[maxn+1]);
+}
+void Encryption(const char* tx) {
+    element_t t;
+    element_init_Zr(t, pairing);
+    element_set_str(t, tx, PBC_CONVERT_BASE);
+    element_t K;
+    element_init_GT(K, pairing);
+    pairing_apply(K, paramsGlobal.params[maxn], paramsGlobal.params[1], pairing);
+    element_pow_zn(K, K, t);
+    element_init_G1(Hdr[0], pairing);
+    element_pow_zn(Hdr[0], paramsGlobal.g, t);
+    element_init_same_as(Hdr[1], paramsGlobal.v);
+    element_set(Hdr[1], paramsGlobal.v);
+
+
+    for (int i=0;i<slgm.size();i++)
+    {
+        int j=slgm[i];
+
+        element_mul(Hdr[1], Hdr[1], paramsGlobal.params[maxn+1-j]);
+    }
+    element_pow_zn(Hdr[1], Hdr[1], t);
+    element_printf("%B\n", Hdr[0]);
+    element_printf("%B\n", Hdr[1]);
+    element_printf("%B\n", t);
+}
+void xulydauvao(string s) {
+    stringstream ss(s);
+    int i;
+
+    while (ss >> i) {
+        slgm.push_back(i);
+        if (ss.peek() == ',')
+            ss.ignore();
+    }
+}
+int main(int argc, char *argv[]) {
+    //Quy ước số 1 là tập giải mã
+    freopen("dauvao.txt", "rt", stdin);
+    string m = "";
+
+    for(int i = 2; i < argc;i++) {
+        m += argv[i];
+    }
+    SETUP_PARAMS();
+    setup();
+    xulydauvao(m);
+    Encryption(argv[1]);
+}
