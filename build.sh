4e031bb897c05628b397aed78f6abd98b9006373
Tuan <trinhsytuan@gmail.com>
2024-02-25 01:00:49 +0700

    update build

--- build.sh
+++ build.sh
@@ -3,4 +3,8 @@
 # Bước 1: Git pull
 echo "Bước 1: Git pull"
 git pull git@github.com:trinhsytuan/DoAnCuoiKhoa_Backend.git
+cd src/Crypto
+g++ -o GiaiMa GiaiMa.cpp -lgmp -lpbc
+g++ -o MaHoa MaHoa.cpp -lgmp -lpbc
+g++ -o MaHoaT MaHoaT.cpp -lgmp -lpbc
 forever restartall
