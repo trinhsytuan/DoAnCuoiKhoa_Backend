d86b76722ab1b9f9c5da5955a4f928a3a632e302
Tuan <trinhsytuan@gmail.com>
2024-03-03 11:02:19 +0700

    update build

--- build.sh
+++ build.sh
@@ -16,13 +16,13 @@ cd /home/tshare.sytuan.net/public_html
 echo "Clone lại"
 rm -rf DoAnCuoiKhoa_FrontEnd
 rm -rf node_modules
+rm *
 git clone git@github.com:trinhsytuan/DoAnCuoiKhoa_FrontEnd.git
 cd DoAnCuoiKhoa_FrontEnd
 echo "Bước 3: yarn install"
 yarn install
 echo "Bước 4: Build web"
 cd ../
-rm *
 cd DoAnCuoiKhoa_FrontEnd
 npm run build
 cd ../
